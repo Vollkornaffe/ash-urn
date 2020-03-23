8b790f7043322ef25384fb95f42a64c5327a445a
Lars Helge Scheel <larshelge.scheel@gmail.com>
2020-03-23 12:07:11 +0100

    Physical device can do everything that is needed I think, user has to decide what device he picks

--- src/base/physical_device.rs
+++ src/base/physical_device.rs
@@ -2,6 +2,7 @@ use crate::error::UrnError;
 use crate::util::vk_to_string;
 
 use ash::version::InstanceV1_0;
+use ash::version::InstanceV1_1;
 
 use super::{QueueFamily, QueueFamilyKey};
 
@@ -89,5 +90,33 @@ impl PhysicalDevice {
             );
         }
         Ok(res)
+    }
+
+    pub fn check_timeline_feature(&self, instance: &ash::Instance) -> bool {
+        let mut timeline_feature = ash::vk::PhysicalDeviceTimelineSemaphoreFeatures::builder()
+            .timeline_semaphore(false)
+            .build();
+        let mut physical_device_features2 = ash::vk::PhysicalDeviceFeatures2::builder().build();
+        let next_ptr = &mut timeline_feature
+            as *mut ash::vk::PhysicalDeviceTimelineSemaphoreFeatures
+            as *mut ash::vk::BaseOutStructure;
+        physical_device_features2.p_next = next_ptr as _;
+        unsafe { instance.get_physical_device_features2(self.0, &mut physical_device_features2) };
+        timeline_feature.timeline_semaphore == 0
+    }
+
+    pub fn query_subgroup_properties(
+        &self,
+        instance: &ash::Instance,
+    ) -> ash::vk::PhysicalDeviceSubgroupProperties {
+        let mut subgroup_properties = ash::vk::PhysicalDeviceSubgroupProperties::builder().build();
+        let mut physical_device_properties2 = ash::vk::PhysicalDeviceProperties2::builder().build();
+        let next_ptr = &mut subgroup_properties as *mut ash::vk::PhysicalDeviceSubgroupProperties
+            as *mut ash::vk::BaseOutStructure;
+        physical_device_properties2.p_next = next_ptr as _;
+        unsafe {
+            instance.get_physical_device_properties2(self.0, &mut physical_device_properties2)
+        };
+        subgroup_properties
     }
 }
