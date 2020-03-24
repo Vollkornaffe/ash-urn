e1a14bfb5e83831e47d62d46b4232dedee630d44
Lars Helge Scheel <larshelge.scheel@gmail.com>
2020-03-24 12:19:41 +0100

    Simple renaming

--- src/commands/buffer.rs
+++ src/commands/buffer.rs
@@ -6,7 +6,7 @@ use ash::version::DeviceV1_0;
 pub struct Buffer(pub ash::vk::CommandBuffer);
 
 impl Buffer {
-    pub fn new(
+    pub fn alloc(
         base: &Base,
         pool: ash::vk::CommandPool,
         name: String,
@@ -24,7 +24,7 @@ impl Buffer {
         Ok(Self(*buffer))
     }
 
-    pub fn new_vec(
+    pub fn alloc_vec(
         base: &Base,
         pool: ash::vk::CommandPool,
         n_buffer: u32,
