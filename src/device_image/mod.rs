6a1b17995c5b419890cb615381a9fc737478d87e
Lars Helge Scheel <larshelge.scheel@gmail.com>
2020-03-28 22:44:56 +0100

    device image creation

--- src/device_image/mod.rs
+++ src/device_image/mod.rs
@@ -1,3 +1,6 @@
+use crate::UrnError;
+use crate::Base;
+
 pub mod image;
 pub mod memory;
 pub mod view;
@@ -6,8 +9,71 @@ pub use image::Image;
 pub use memory::Memory;
 pub use view::View;
 
+use image::ImageSettings;
+use memory::MemorySettings;
+use view::ViewSettings;
+
 pub struct DeviceImage {
     pub image: Image,
     pub memory: Memory,
     pub view: View,
+}
+
+pub struct DeviceImageSettings {
+    width: u32,
+    height: u32,
+    format: ash::vk::Format,
+    tiling: ash::vk::ImageTiling,
+    usage: ash::vk::ImageUsageFlags,
+    properties: ash::vk::MemoryPropertyFlags,
+    image: ash::vk::Image,
+    aspect_flags: ash::vk::ImageAspectFlags,
+    name: String,
+}
+
+impl DeviceImage {
+
+    pub fn new(
+        base: &Base,
+        settings: &DeviceImageSettings,
+    ) -> Result<Self, UrnError> {
+
+        let image = Image::new(
+            base,
+            &ImageSettings {
+                width: settings.width,
+                height: settings.height,
+                format: settings.format,
+                tiling: settings.tiling,
+                usage: settings.usage,
+                name: format!("{}Image", settings.name.clone()),
+            },
+        )?;
+
+        let memory = Memory::alloc(
+            base,
+            &MemorySettings {
+                properties: settings.properties,
+                image: image.0,
+                name: format!("{}Memory", settings.name.clone()),
+            },
+        )?;
+
+        let view = View::new(
+            base,
+            &ViewSettings {
+                image: image.0,
+                format: settings.format,
+                aspect_flags: settings.aspect_flags,
+                name: format!("{}View", settings.name.clone()),
+            },
+        )?;
+
+        Ok(Self {
+            image,
+            memory,
+            view,
+        })
+    }
+
 }
