8272e1e3e5d5cc39ef8c3a076e441fa728ca0cce
Lars Helge Scheel <larshelge.scheel@gmail.com>
2020-03-29 00:10:59 +0100

    Index buffer creation

--- src/transfer/mod.rs
+++ src/transfer/mod.rs
@@ -1,7 +1,9 @@
 pub mod copy;
 pub mod staging;
 pub mod vertex;
+pub mod index;
 
 pub use copy::{copy_buffer_to_buffer, copy_buffer_to_image};
 pub use staging::create_staging_buffer;
 pub use vertex::create_vertex_buffer;
+pub use index::create_index_buffer;
