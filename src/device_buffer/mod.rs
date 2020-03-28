45f524cb28ea84718ca62d2d450e00da30b9f955
Lars Helge Scheel <larshelge.scheel@gmail.com>
2020-03-28 22:26:57 +0100

    device buffer creation

--- src/device_buffer/mod.rs
+++ src/device_buffer/mod.rs
@@ -1,10 +1,55 @@
+use crate::UrnError;
+use crate::Base;
+
 mod buffer;
 mod memory;
 
 pub use buffer::Buffer;
 pub use memory::Memory;
 
+use buffer::BufferSettings;
+use memory::MemorySettings;
+
+
 pub struct DeviceBuffer {
     pub buffer: Buffer,
     pub memory: Memory,
+}
+
+pub struct DeviceBufferSettings {
+    pub size: ash::vk::DeviceSize,
+    pub usage: ash::vk::BufferUsageFlags,
+    pub properties: ash::vk::MemoryPropertyFlags,
+    pub name: String,
+}
+
+impl DeviceBuffer {
+
+    pub fn new(
+        base: &Base,
+        settings: &DeviceBufferSettings,
+    ) -> Result<Self, UrnError> {
+        let buffer = Buffer::new(
+            base,
+            &BufferSettings {
+                size: settings.size,
+                usage: settings.usage,
+                name: format!("{}Buffer", settings.name.clone()),
+            },
+        )?;
+
+        let memory = Memory::alloc(
+            base,
+            &MemorySettings {
+                properties: settings.properties,
+                buffer: buffer.0,
+                name: format!("{}Memory", settings.name.clone()),
+            },
+        )?;
+
+        Ok(Self {
+            buffer,
+            memory,
+        })
+    }
 }
