9bcf261f7b232b41dbed6809de560d2bb4a47ed5
Lars Helge Scheel <larshelge.scheel@gmail.com>
2020-04-08 20:51:27 +0200

    Moved advance_frame, for some reason, resizing is sometimes stuck.

--- examples/basic_graphics/main.rs
+++ examples/basic_graphics/main.rs
@@ -1,16 +1,14 @@
 pub mod error;
 pub mod sdl;
+pub mod run;
+pub mod setup;
 
 pub use error::AppError;
 pub use sdl::SDL;
 pub use setup::Setup;
 
-mod run;
-mod setup;
-
 use ash_urn::memory_alignment::Align16;
 use ash_urn::wait_device_idle;
-use ash_urn::Base;
 use ash_urn::Mesh;
 
 #[repr(C)]
@@ -18,58 +16,6 @@ struct UBO {
     model: Align16<cgmath::Matrix4<f32>>,
     view: Align16<cgmath::Matrix4<f32>>,
     proj: Align16<cgmath::Matrix4<f32>>,
-}
-
-fn advance_frame(
-    base: &Base,
-    setup: &Setup,
-    start_instant: &std::time::Instant,
-    frame: &mut u64,
-) -> Result<(), AppError> {
-    // wait for last frame to complete rendering before submitting.
-    setup.timeline.wait(&base, *frame)?;
-
-    // acquire an image
-    let image_index = run::next_image::aquire(&setup.swap_chain, &setup.semaphore_image_acquired)?;
-
-    // only waiting on fence because the validation layers don't get timelines
-    // if running without validation, the fence is not needed.
-    setup.fence_rendering_finished.wait(&base)?;
-    setup.fence_rendering_finished.reset(&base)?;
-
-    // update model matrix based on time
-    run::uniform_buffer::update(
-        &base,
-        &setup.uniform_buffers[image_index as usize],
-        &setup.swap_chain,
-        &start_instant,
-    )?;
-
-    // submit the rendering commands to the combined queue
-    // waiting on image_aquired, signaling rendering_finished
-    run::render::submit(
-        &base,
-        &setup.graphics_command,
-        &setup.timeline,
-        &setup.semaphore_image_acquired,
-        &setup.semaphore_rendering_finished,
-        &setup.fence_rendering_finished,
-        *frame,
-        image_index,
-    )?;
-
-    // submit to the present queue via the swap chain loader
-    // waiting on rendering_finished, doesn't signal anything
-    run::present::submit(
-        &setup.swap_chain,
-        &setup.graphics_command,
-        &setup.semaphore_rendering_finished,
-        image_index,
-    )?;
-
-    *frame += 1;
-
-    Ok(())
 }
 
 fn main() {
@@ -134,7 +80,7 @@ fn main() {
             }
 
             // check if the iteration failed due to resize
-            match advance_frame(
+            match run::advance_frame(
                 &base,
                 &setup,
                 &start_instant,
--- examples/basic_graphics/run/mod.rs
+++ examples/basic_graphics/run/mod.rs
@@ -1,4 +1,62 @@
+use crate::AppError;
+use crate::Setup;
+
 pub mod next_image;
 pub mod present;
 pub mod render;
 pub mod uniform_buffer;
+
+use ash_urn::Base;
+
+pub fn advance_frame(
+    base: &Base,
+    setup: &Setup,
+    start_instant: &std::time::Instant,
+    frame: &mut u64,
+) -> Result<(), AppError> {
+
+    // acquire an image
+    let image_index = next_image::aquire(&setup.swap_chain, &setup.semaphore_image_acquired)?;
+
+    // wait for last frame to complete rendering before submitting.
+    setup.timeline.wait(&base, *frame)?;
+
+    // only waiting on fence because the validation layers don't get timelines
+    // if running without validation, the fence is not needed.
+    setup.fence_rendering_finished.wait(&base)?;
+    setup.fence_rendering_finished.reset(&base)?;
+
+    // update model matrix based on time
+    uniform_buffer::update(
+        &base,
+        &setup.uniform_buffers[image_index as usize],
+        &setup.swap_chain,
+        &start_instant,
+    )?;
+
+    // submit the rendering commands to the combined queue
+    // waiting on image_aquired, signaling rendering_finished
+    render::submit(
+        &base,
+        &setup.graphics_command,
+        &setup.timeline,
+        &setup.semaphore_image_acquired,
+        &setup.semaphore_rendering_finished,
+        &setup.fence_rendering_finished,
+        *frame,
+        image_index,
+    )?;
+
+    // submit to the present queue via the swap chain loader
+    // waiting on rendering_finished, doesn't signal anything
+    present::submit(
+        &setup.swap_chain,
+        &setup.graphics_command,
+        &setup.semaphore_rendering_finished,
+        image_index,
+    )?;
+
+    *frame += 1;
+
+    Ok(())
+}
