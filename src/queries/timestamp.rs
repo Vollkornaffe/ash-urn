52104d8bcb8291f93bfc0528720da471709f961c
Lars Helge Scheel <larshelge.scheel@gmail.com>
2020-04-10 21:22:14 +0200

    Added some more utility functions to timestamp, can't reset pool from host.

--- src/queries/timestamp.rs
+++ src/queries/timestamp.rs
@@ -2,6 +2,7 @@ use crate::UrnError;
 use crate::Base;
 
 use ash::version::DeviceV1_0;
+use ash::version::DeviceV1_2;
 
 pub struct Timestamp {
     pub pool: ash::vk::QueryPool,
@@ -26,10 +27,67 @@ impl Timestamp {
         };
         base.name_object(query_pool, name)?;
 
-        Ok(Self {
+        // reset the pool just to be sure
+        let timestamp = Self {
             pool: query_pool,
             names: stamp_names,
-        })
+        };
+
+        Ok(timestamp)
+    }
+    
+    pub fn reset_pool(&self, base: &Base, command_buffer: ash::vk::CommandBuffer) {
+        unsafe {
+            base.logical_device.0.cmd_reset_query_pool(
+                command_buffer,
+                self.pool,
+                0,
+                self.names.len() as u32,
+            )
+        }
+    }
+
+    pub fn mark(
+        &self,
+        base: &Base,
+        command_buffer: ash::vk::CommandBuffer,
+        pipeline_stage: ash::vk::PipelineStageFlags,
+        name: &str
+    ) {
+        let query_idx = self
+            .names
+            .iter()
+            .position(|n| n == &name.to_string())
+            .expect("Timestamp name not found.") as u32;
+        unsafe {
+            base
+                .logical_device
+                .0
+                .cmd_write_timestamp(
+                    command_buffer,
+                    pipeline_stage,
+                    self.pool,
+                    query_idx,
+                );
+        }
+    }
+
+    pub fn query_all(&self, base: &Base) -> Result<Vec<u64>, UrnError> {
+        let mut data: Vec<u64> = Vec::new();
+        data.resize(self.names.len(), 0);
+        unsafe {
+            base
+                .logical_device
+                .0
+                .get_query_pool_results(
+                    self.pool,
+                    0,
+                    self.names.len() as u32,
+                    &mut data,
+                    ash::vk::QueryResultFlags::TYPE_64,
+                )?;
+        }
+        Ok(data)
     }
 
     pub fn destroy(&self, base: &Base) {
