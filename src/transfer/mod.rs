0e79cd9bd7a5e845a4970996cc60f3db087ea55c
Lars Helge Scheel <larshelge.scheel@gmail.com>
2020-04-26 19:55:23 +0200

    Forgot to add to mod.rs

--- src/transfer/mod.rs
+++ src/transfer/mod.rs
@@ -10,6 +10,7 @@ pub use copy::{copy_buffer_to_buffer, copy_buffer_to_image};
 pub use index::create_index_device_buffer;
 pub use staging::create_staging_device_buffer;
 pub use storage::create_storage_device_buffer;
+pub use storage::create_storage_device_buffer_uninitialized;
 pub use texture::create_texture_device_image;
 pub use vertex::create_vertex_device_buffer;
 pub use vertex::create_vertex_storage_device_buffer;
